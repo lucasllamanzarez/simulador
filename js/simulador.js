6929b88fab1e4c34fb335e43675b0d15e81f732d
Lucas Llamanzarez <lucas.llamanzarez@gmail.com>
2022-06-16 13:05:17 -0300

    Desafio Pre Entrega - Asalto al castillo 2da parte

--- js/simulador.js
+++ js/simulador.js
@@ -1,41 +1,41 @@
-///version refactorizada
+///version refactorizada.
 
-//Vida Castillo
+//Vida Castillo.
         let castle = 100;
 
-//Posiciones
+//Posiciones.
         const marcador = [{ jugador: "Matias", rondas: 15 }, { jugador: "Bernardo", rondas: 20 }, { jugador: "Leandro", rondas: 16 }];
 
-//Inserta Nuevo Jugador
+//Inserta Nuevo Jugador.
         function jugador(jugador, rondas) {
                 this.jugador = jugador;
         this.rondas = rondas;
 }
 
-//Funcion Push
+//Funcion Push.
         function agregar() {
                 marcador.push(nuevoJugador);
 }
 
 
-//Funcion Confirm. Si se obtiene valor true, inicia el While
+//Funcion Confirm. Si se obtiene valor true, inicia el While.
         let jugador1 = confirm("🏰¿Queres comenzar el juego?🏰");
 
-// Genera Ronda
+// Genera Ronda.
         let round = 0;
 
         while (jugador1) {
 
-///Si es castillo tiene mas de 0 de vida, sigue el juego
+///Si es castillo tiene mas de 0 de vida, sigue el juego.
         if (castle > 0) {
 
                 let jugador1Ataca = confirm(`¿Desea atacar?⚔️`);
 
         if (jugador1Ataca) {
-            //Variable de ataque random
+            //Variable de ataque random.
                 let Atk = Math.round(Math.random() * 25);
 
-                        //Funcion de Ataque Local
+                        //Funcion de Ataque Local.
                                 function ataque() {
                                         castle = castle - Atk;
                                         
@@ -47,7 +47,7 @@
                 ataque(castle, Atk);
         alert(`¡Has atacado al castillo! ⚔️ Puntos de vida restantes: ${castle} 🏰`);
 
-                //Incremento la ronda en 1 siempre
+                //Incremento la ronda en 1 siempre.
                 round++;
 
         } else {
@@ -62,17 +62,17 @@
 
                 alert(`⚔️¡Has destruido el castillo!⚔️`);
 
-                //Variables para guardar datos y luego cargarlos
+                //Variables para guardar datos y luego cargarlos.
                         let rondas = round;
                                 let playerC = prompt("Ingresa tu nombre para sumarte al Ranking");
                         let rondaC = rondas;
 
                 nuevoJugador = new jugador(playerC, rondaC);
 
-        //Llamo funcion para guardar los datos        
+        //Llamo funcion para guardar los datos.     
         agregar();
 
-                //Ordeno Marcador y muestro los 3 primeros puestos
+                //Ordeno Marcador y muestro los 3 primeros puestos.
                         marcador.sort((a, b) => a.rondas - b.rondas);
                 alert(`RANKING DE JUGADORES:
 
@@ -82,12 +82,12 @@
 
                 3 ${marcador[2].jugador} - ${marcador[2].rondas}`);
 
-        //Reinicio juego en caso de que la respuesta sea True
+        //Reinicio juego en caso de que la respuesta sea True.
                 let playAgain = confirm(`¿Queres jugar de nuevo?`);
 
                         if (playAgain) {
 
-                //Resetea Vida y Rondas
+                //Resetea Vida y Rondas.
                         castle = 100;
                         round = 0;
 
